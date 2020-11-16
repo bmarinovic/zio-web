12ba8f047b3b98a9d5999afc18768f539376150f
bmarinovic <blmarino@fesb.hr>
2020-11-16 19:40:48 +0100

    Compare derived schema with expected one (#69)

--- core/src/test/scala/zio/web/schema/DeriveSchemaSpec.scala
+++ core/src/test/scala/zio/web/schema/DeriveSchemaSpec.scala
@@ -1,7 +1,8 @@
 package zio.web.schema
 
-import zio.test.Assertion.equalTo
+import zio.Chunk
 import zio.test.{ DefaultRunnableSpec, assert, suite, test }
+import zio.web.schema.SchemaAssertions.hasSameSchema
 
 object DeriveSchemaSpec extends DefaultRunnableSpec {
 
@@ -18,97 +19,59 @@ object DeriveSchemaSpec extends DefaultRunnableSpec {
 
       val userIdSchema: Schema[UserId] = DeriveSchema.gen
 
-      assert((userIdSchema: Schema[_]) match {
-        case Schema.Transform(codec, _, _) =>
-          (codec: Schema[_]) match {
-            case Schema.Record(structure) =>
-              structure("id") match {
-                case Schema.Primitive(StandardType.StringType) => true
-                case _                                         => false
-              }
-            case _ => false
-          }
-        case _ => false
-      })(equalTo(true))
+      val expectedSchema: Schema[UserId] = Schema.Transform[Map[String, Any], UserId](
+        Schema.Record(Map("id" -> Schema.Primitive(StandardType.StringType))),
+        map => Right(UserId(map("id").asInstanceOf[String])), // ignored in equality comparison
+        (userId: UserId) => Right(Map("id" -> userId.id)) // ignored in equality comparison
+      )
+
+      assert(userIdSchema)(hasSameSchema(expectedSchema))
     },
     test("DeriveSchema correctly derives schema for complex ADT") {
 
       val statusSchema: Schema[Status] = DeriveSchema.gen[Status]
 
-      assert((statusSchema: Schema[_]) match {
-        case Schema.Transform(codec, _, _) =>
-          (codec: Schema[_]) match {
-            case Schema.Enumeration(enumStructure) =>
-              val okBranch = enumStructure("Ok") match {
-                case Schema.Transform(okSchema, _, _) =>
-                  (okSchema: Schema[_]) match {
-                    case Schema.Record(responseStructure) =>
-                      responseStructure("response") match {
-                        case Schema.Transform(responseSchema, _, _) =>
-                          (responseSchema: Schema[_]) match {
-                            case Schema.Sequence(elementSchema) =>
-                              (elementSchema: Schema[_]) match {
-                                case Schema.Primitive(StandardType.StringType) => true
-                                case _                                         => false
-                              }
-                            case _ => false
-                          }
-                        case _ => false
-                      }
-                    case _ => false
-                  }
-                case _ => false
-              }
+      val expectedSchema: Schema[Status] =
+        Schema.Transform[Map[String, Any], Status](
+          Schema.Enumeration(
+            Map(
+              "Ok" -> Schema.Transform[Map[String, Any], Ok](
+                Schema.Record(
+                  Map(
+                    "response" -> Schema.Transform[Chunk[String], List[String]](
+                      Schema.Sequence(Schema.Primitive(StandardType.StringType)),
+                      chunk => Right(chunk.toList),                   // ignored in equality comparison
+                      elements => Right(Chunk.fromIterable(elements)) // ignored in equality comparison
+                    )
+                  )
+                ),
+                map => Right(Ok(response = List(map.toString))), // ignored in equality comparison
+                ok => Right(Map("response" -> ok.response)) // ignored in equality comparison
+              ),
+              "Failed" -> Schema.Transform[Map[String, Any], Failed](
+                Schema.Record(
+                  Map(
+                    "code"                  -> Schema.Primitive(StandardType.IntType),
+                    "reason"                -> Schema.Primitive(StandardType.StringType),
+                    "additionalExplanation" -> Schema.option(Schema.Primitive(StandardType.StringType)),
+                    "remark"                -> Schema.Primitive(StandardType.StringType)
+                  )
+                ),
+                _ => Right(Failed(1, "a", None)), // ignored in equality comparison
+                failed => Right(Map("code" -> failed.code, "reason" -> failed.reason)) // ignored in equality comparison
+              ),
+              "Pending" -> Schema.Transform[Map[String, Any], Pending.type](
+                Schema.Record(Map()),
+                _ => Right(Pending), // ignored in equality comparison
+                _ => Right(Map())    // ignored in equality comparison
+              )
+            )
+          ),
+          _ => Right(Pending), // ignored in equality comparison
+          _ => Right(Map("Pending" -> Pending)) // ignored in equality comparison
+        )
 
-              val failedBranch = enumStructure("Failed") match {
-                case Schema.Transform(failedSchema, _, _) =>
-                  (failedSchema: Schema[_]) match {
-                    case Schema.Record(failedStructure) =>
-                      val code = failedStructure("code") match {
-                        case Schema.Primitive(StandardType.IntType) => true
-                        case _                                      => false
-                      }
-
-                      val reason = failedStructure("reason") match {
-                        case Schema.Primitive(StandardType.StringType) => true
-                        case _                                         => false
-                      }
-
-                      val additionalExplanation = failedStructure("additionalExplanation") match {
-                        case Schema.Optional(additionalExplanationSchema) =>
-                          (additionalExplanationSchema: Schema[_]) match {
-                            case Schema.Primitive(StandardType.StringType) => true
-                            case _                                         => false
-                          }
-                        case _ => false
-                      }
-
-                      val remark = failedStructure("remark") match {
-                        case Schema.Primitive(StandardType.StringType) => true
-                        case _                                         => false
-                      }
-
-                      code && reason && additionalExplanation && remark
-
-                    case _ => false
-                  }
-                case _ => false
-              }
-
-              val pendingBranch = enumStructure("Pending") match {
-                case Schema.Transform(pendingSchema, _, _) =>
-                  (pendingSchema: Schema[_]) match {
-                    case Schema.Record(pendingStructure) => pendingStructure.isEmpty
-                    case _                               => false
-                  }
-                case _ => false
-              }
-
-              okBranch && failedBranch && pendingBranch
-            case _ => false
-          }
-        case _ => false
-      })(equalTo(true))
+      assert(statusSchema)(hasSameSchema(expectedSchema))
     }
   )
 }
